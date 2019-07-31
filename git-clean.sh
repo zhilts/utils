05e7e0803bd11aed45e59bbe0d8b2df3901ce6d7
Sergey Zhiltsov <zhiltsov.s.i@gmail.com>
2019-07-31 22:30:21 +0300

    Fix git-clean

--- git-clean.sh
+++ git-clean.sh
@@ -11,6 +11,6 @@ COMMIT=$(git show --format=%H ${BRANCH})
 
 git branch --merged | egrep -v "(^\*|master|develop|release|HEAD)" | xargs git branch -d
 # push to remote
-WEEK_AGO=python -c "from datetime import date, timedelta; print(date.today()-timedelta(days=7))" 
+WEEK_AGO=$(python -c "from datetime import date, timedelta; print(date.today()-timedelta(days=7))")
 git branch -r --format="${WEEK_AGO} %(committerdate:short) %(refname:short)" --merged | sed 's/origin\///g' | egrep -v "(^\*|master|develop|release|HEAD)" | awk '$1 > $2' | awk '{ print $3 }' | xargs git push origin --delete 
 git prune
