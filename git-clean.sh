6593ebe9b3019477e154c8c65d1bcdcaad057fc8
Sergey Zhiltsov <zhiltsov.s.i@gmail.com>
2019-07-31 18:29:17 +0300

    Add cleaning old branches

--- git-clean.sh
+++ git-clean.sh
@@ -9,7 +9,8 @@ fi
 
 COMMIT=$(git show --format=%H ${BRANCH})
 
-git branch --merged | egrep -v "(^\*|master|develop|release)" | xargs git branch -d
+git branch --merged | egrep -v "(^\*|master|develop|release|HEAD)" | xargs git branch -d
 # push to remote
-git branch -r --merged  | egrep -v "(^\*|master|develop|release)" | sed 's/origin\///g' | xargs git push origin --delete 
+WEEK_AGO=python -c "from datetime import date, timedelta; print(date.today()-timedelta(days=7))" 
+git branch -r --format="${WEEK_AGO} %(committerdate:short) %(refname:short)" --merged | sed 's/origin\///g' | egrep -v "(^\*|master|develop|release|HEAD)" | awk '$1 > $2' | awk '{ print $3 }' | xargs git push origin --delete 
 git prune
