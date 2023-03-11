11e8b93a9f3fc794d9ab09a60bff9bcd750274b6
Sergey Zhiltsov <zhiltsov.s.i@gmail.com>
2023-03-11 13:30:26 +0100

    Push to remote optionally

--- git-clean.sh
+++ git-clean.sh
@@ -10,13 +10,13 @@ fi
 COMMIT=$(git show --format=%H ${BRANCH})
 
 git fetch --prune
-git branch --merged | egrep -v "(^\*|master|develop|release|HEAD)" | xargs git branch -d
+git branch --merged | egrep -v "(^\*|main|master|develop|release|HEAD)" | xargs git branch -d
 # push to remote
 if [ "$2" == "-r" ]
 then
 	echo "Pushing to remote"
 	WEEK_AGO=$(python -c "from datetime import date, timedelta; print(date.today()-timedelta(days=30))")
-	git branch -r --format="${WEEK_AGO} %(committerdate:short) %(refname:short)" --merged | sed 's/origin\///g' | egrep -v "(^\*|master|develop|release|HEAD)" | awk '$1 > $2' | awk '{ print $3 }' | xargs git push origin --delete 
+	git branch -r --format="${WEEK_AGO} %(committerdate:short) %(refname:short)" --merged | sed 's/origin\///g' | egrep -v "(^\*|main|master|develop|release|HEAD)" | awk '$1 > $2' | awk '{ print $3 }' | xargs git push origin --delete
 	git prune
 else
 	echo "Skipping remote"
